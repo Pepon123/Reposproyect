6f26a96a3067afb78e83f454851d44eb512f3376
Jose Cordero <Jose Cordero@PEPE2>
2021-05-07 14:11:52 -0500

    Modificaciones

---
+++ pruebagit/src/pruebagit/Pruebagit.java
@@ -0,0 +1,16 @@
+package pruebagit;
+
+
+public class Pruebagit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //este es mi primer commit
+        
+        
+    }
+    
+}
